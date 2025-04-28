48da9a81976d8493f8171100f52ab9ec43a381cc
ece-mohammad <kuro.ece@gmail.com>
2025-04-28 15:13:56 +0300

    Version 0.1: - Added basic implementation of the requote tool
    Todo:
    - tests

---
+++ tests/test_requote.py
@@ -0,0 +1,27 @@
+import pytest
+from requote import (
+    toml_loader,
+    yaml_loader,
+    json_loader,
+    read_stdin,
+    split_quotes,
+    quote_string,
+    load_conf,
+    get_style,
+    requote_code,
+)
+
+
+# test quoting quoting 
+
+# test split quotes
+
+# test requoting
+class TestRequoteCode:
+    ...
+
+# test configuration parsing
+
+# test configuration loading from files
+
+# test reading from stdin
